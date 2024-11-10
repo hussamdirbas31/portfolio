1af02ea62e940a33e28be913a5e1b008c94c25b1
hussamdirbas31 <hussamdirbas11@gmail.com>
2024-11-11 02:43:13 +0300

    add

--- utils/data/projectsImages.js
+++ utils/data/projectsImages.js
@@ -1,7 +1,6 @@
 
 import ecommerce from '@/public/projects/ecommerce.jpg'
 import ody from '../../public/projects/crypto.jpg'
-import weather from '../../public/projects/weather.jpg'
 export const projectsData = [
     {
       title: "Ecommerce",
