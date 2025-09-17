65109a95827433fabb5ea14c3f294179d3336891
hussamdirbas31 <hussamdirbas11@gmail.com>
2025-09-17 03:04:58 +0300

    update data.ts of projects

--- utils/data/data.ts
+++ utils/data/data.ts
@@ -3,7 +3,10 @@ import ecommerce from '../../public/ecommerce.jpg';
 import cold from '../../public/cold.jpeg';
 import crypto from '../../public/crypto.jpg';
 import chatify from '../../public/chatify.jpg';
-import pixels from '../../public/pixelcraft.jpg'
+import pixels from '../../public/pixelcraft.jpg';
+import nafir from '../../public/nafir.jpg';
+import ghiath from '../../public/ghiath.jpg';
+
 interface ProjectData {
   title: string;
   description: string;
@@ -13,6 +16,20 @@ interface ProjectData {
 }
 
 export const projectsData: ProjectData[] = [
+  {
+    title: "Nafir",
+    description: "A Sudanese initiative supporting skill development and professional networking for Sudanese individuals.",
+    imageUrl: nafir,
+    link: 'https://portal.nafir.net/',
+    tech: ["Next js", "Framer Motion", "Zod"]
+  },
+  {
+    title: "Motion Designer Portfolio",
+    description: "Portfolio website for a motion designer with smooth animations and interactions.",
+    imageUrl: ghiath,
+    link: 'https://ghiathdaheer.vercel.app/',
+    tech: ["Next js", "Framer Motion", "Lenis"]
+  },
   {
     title: "Ecommerce",
     description: "Clothing store (Dashboard, add to cart)",
